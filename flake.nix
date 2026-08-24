b9db3062e090b8de5171f20a795c94f87b318828
PhreshBrread <crustydog9@gmail.com>
2026-08-24 12:22:08 +1000

    Add flake-utils

--- flake.nix
+++ flake.nix
@@ -1,8 +1,12 @@
 {
   description        = "Zooma flake";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
+  inputs = {
+    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
