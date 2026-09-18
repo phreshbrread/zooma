0732bc89e36f3f5c30c493c136c6ddbce4cf3aef
PhreshBrread <crustydog9@gmail.com>
2026-09-19 07:06:27 +1000

    Fix min and max spotlight sizes being swapped; use .clamp() for size bounds

--- src/main.rs
+++ src/main.rs
@@ -219,21 +219,16 @@ fn main() {
         }
 
         if shift_key_down {
-            let max_circle_size = render_size_avg * 0.02;
-            let min_circle_size = render_size_avg / 3.0;
+            let min_circle_size = render_size_avg * 0.02;
+            let max_circle_size = render_size_avg / 3.0;
 
             if win.get_mouse_wheel_move() > 0.0 {
-                // TODO: Use circle_size.clamp() instead
-                if circle_size < max_circle_size {
-                    circle_size = max_circle_size;
-                }
                 circle_size -= 10.0;
             } else if win.get_mouse_wheel_move() < 0.0 {
-                if circle_size > min_circle_size {
-                    circle_size = min_circle_size;
-                }
                 circle_size += 10.0;
             }
+
+            circle_size = circle_size.clamp(min_circle_size, max_circle_size);
         }
     }
 }
